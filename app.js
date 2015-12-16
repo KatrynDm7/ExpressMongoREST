90781543a7452e0b3a44d84c8b362f018a36272f
Katryn Dmytruk <kevind@mail.ru>
2015-12-16 05:20:40 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var config = require('./config/config');
 var mongoose = require('mongoose');
@@ -25,7 +23,6 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 require('./routes/routes')(app);
